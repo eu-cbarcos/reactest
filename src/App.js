64c5eb2db933be095e238622163ce15d77c520d5
cbarcos <cbarcos@eluniverso.com>
2019-11-06 13:15:08 -0800

    Mostrando data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Input from "./components/Input";
 import Textarea from "./components/Textarea"
 
@@ -9,35 +8,53 @@ class App extends React.Component {
     super(props);
     this.state = {
       titulo : "",
-      contenido : ""
+      contenido : "",
+      entradas: [],
     }
     this.onChange = this.onChange.bind(this);
+    this.crearEntrada = this.crearEntrada.bind(this);
   }
   onChange (e) {
     this.setState({
-      [e.target.id] : e.target.value
+      [e.target.id] : e.target.value,
+    });
+  }
+  crearEntrada (e) {
+    e.preventDefault();
+    console.log("submitting...");
+    this.setState({
+      entradas: [...this.state.entradas,{
+        titulo: this.state.titulo,
+        contenido: this.state.contenido,
+      }],
+      titulo: '',
+      contenido: '',
     });
   }
   render () {
     return (
       <div>
         <aside>
-          <form onClick={this.crearEntrada}>
+          <form onSubmit={this.crearEntrada} className="formulario">
             <Input id="titulo" name="titulo" label="Titulo" onChange={this.onChange} value={this.state.titulo}/>
             <Textarea id="contenido" name="contenido" label="Contenido" onChange={this.onChange} value={this.state.contenido}/>
+            <button>Crear entrada</button>
           </form>
         </aside>
+        <section>
+          <ul>
+            {
+              this.state.entradas.map( (entrada,indice) => (
+                <li key={indice}>
+                  <h3>{entrada.titulo}</h3>
+                  <p>{entrada.contenido}</p>
+                </li>
+              ))
+            }
+          </ul>
+        </section>
       </div>
     );
-    
   }
 }
-
 export default App;
-
-/*
-<div>
-              <label htmlFor="contenido">Contenido</label>
-              <textarea id="contenido" name="contenido" onChange={this.onChange} value={this.state.contenido}></textarea>
-            </div>
-*/
